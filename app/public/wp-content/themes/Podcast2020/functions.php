b1224981db572f178fabb0d7c66a122bc10e0c9c
David Vaughan <CreativeWavelength@Creative-Wavelength.local>
2020-04-30 23:05:43 +0200

    Some working links header

--- app/public/wp-content/themes/Podcast2020/functions.php
+++ app/public/wp-content/themes/Podcast2020/functions.php
@@ -1,8 +1,18 @@
 <?php
 
 function podcast_css() {
+wp_enqueue_script('js',get_theme_file_uri('/js/scripts-bundled.js'), NULL /* Has Dependicies? */ , '1.0' /*Version Number */ , true /* Wordpress wants to know if you want to load this file right before the bottom */ );
+    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('whatever', get_stylesheet_uri());
 }
 
 add_action('wp_enqueue_scripts','podcast_css'); 
+
+function university_features() {
+    add_theme_support('title-tag');
+} 
+
+add_action('after_setup_theme', 'university_features')
 ?>
+
--- app/public/wp-content/themes/Podcast2020/single.php
+++ app/public/wp-content/themes/Podcast2020/single.php
@@ -6,8 +6,7 @@ get_header();
 while(have_posts()) {
      the_post(); ?>
      <h2> <?php the_title();?> </h2>
-     <?php the_content(); ?>
-     <?php 
+     <?php the_content();
 }
 
 get_footer();
